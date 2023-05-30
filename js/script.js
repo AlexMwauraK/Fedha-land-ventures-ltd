7fbf2d72aee1c73fcd8f5d1bfc8258527e95cf06
Alex Mwaura <alecsmwaura@gmail.com>
2023-05-30 15:44:22 +0300

    update

--- js/script.js
+++ js/script.js
@@ -62,3 +62,28 @@ if(n < 1){
 myslide[counter - 1].style.display = "block";
 dot[counter - 1].className += " active";
 }
+
+// Testimonial slide 
+$('.testimonials-container').owlCarousel({
+  loop:true,
+  autoplay:true,
+  autoplayTimeout:6000,
+  margin:10,
+  nav:true,
+  navText:["<i class='fa-solid fa-arrow-left'></i>",
+           "<i class='fa-solid fa-arrow-right'></i>"],
+  responsive:{
+      0:{
+          items:1,
+          nav:false
+      },
+      600:{
+          items:1,
+          nav:true
+      },
+      768:{
+          items:2
+      },
+  }
+})
+
